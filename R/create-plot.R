9172a2e82ceed4902393249a09107dcf6c05be5f
Mannion-Niall <Niall.Mannion@nisra.gov.uk>
2023-02-14 09:21:49 +0000

    klk

--- R/create-plot.R
+++ R/create-plot.R
@@ -12,11 +12,11 @@ df = read_csv("data/weatherAUS.csv")
 city = "Brisbane"
 
 scatter_plot = 
-df %>%
+  df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red") +
-  ggtitle("Title")
+  geom_point(colour = "yellow") + 
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
